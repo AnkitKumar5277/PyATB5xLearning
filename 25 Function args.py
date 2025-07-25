b4fc9f99e6141848854c2b274848882f3996ff49
Automation Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-07-25 21:42:20 +0530

    Create 25 Function args.py

---
+++ 25 Function args.py
@@ -0,0 +1,10 @@
+def print_mul_arguments(*args):
+    # *args -> List
+    for i in args:
+        print(i)
+
+
+print_mul_arguments("pramod1\n")
+
+print_mul_arguments("pramod", "amit", "lucky")
+print_mul_arguments("amit", 10, True, False, "PRAMOD")
