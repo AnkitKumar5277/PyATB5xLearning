f9a2f1cad3912fe015809917289fe01d6f734238
Ankit Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-09-12 16:28:06 +0530

    Update 6.py

--- Interview/6.py
+++ Interview/6.py
@@ -1,1 +1,16 @@
+# Creating variables
+name = "Ankit"      # string type variable
+age = 21            # integer type variable
+height = 5.9        # float type variable
+is_student = True   # boolean type variable
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Are you a student?", is_student)
+
+# You can change the value of a variable
+age = 22
+print("Updated Age:", age)
 
