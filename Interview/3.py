c44f61393c53814b10d5d10e58f8e62d21a8b611
Ankit Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-09-12 16:21:18 +0530

    Create 3.py

---
+++ Interview/3.py
@@ -0,0 +1,10 @@
+def addNumbers(x,y):
+  return x+y
+print(addNumbers(5,10))
+
+# Function name should be lowercase_with_underscores
+def add_numbers(x, y):
+    return x + y  # Space around operators
+
+# Blank line between functions improves readability
+print(add_numbers(5, 10))
