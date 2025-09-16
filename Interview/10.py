cf199831600a8bc3b3a249939d9e634d7bc71912
Ankit Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-09-16 23:12:04 +0530

    Create 10.py

---
+++ Interview/10.py
@@ -0,0 +1,14 @@
+name = 'Alice'
+message = "Hello"
+
+# Indexing & Slicing:
+my_string = "Hello"
+print(my_string[0])   # H
+print(my_string[0:3]) # Hel
+
+# Concatenation & Formatting:
+first = "Alice"
+last = "Smith"
+print(first + " " + last)  # Alice Smith
+age = 30
+print(f"My name is {first} {last} and I am {age} years old").
